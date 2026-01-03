e0afc52f1abf34d18049e1b36663de955577d37e
athulsnayak <103585243+athulsnayak@users.noreply.github.com>
2026-01-03 16:14:15 +0530

    Remove comment on future data extension
    Removed comment about extending data till day 49.

---
+++ standardData.js
@@ -0,0 +1,13 @@
+export const standardData = {
+  1: { feedIntake: 15, cumFeed: 15, bodyWt: 55, fcr: 0.27 },
+  2: { feedIntake: 18, cumFeed: 33, bodyWt: 70, fcr: 0.47 },
+  3: { feedIntake: 21, cumFeed: 54, bodyWt: 90, fcr: 0.60 },
+  4: { feedIntake: 24, cumFeed: 78, bodyWt: 110, fcr: 0.71 },
+  5: { feedIntake: 28, cumFeed: 106, bodyWt: 135, fcr: 0.79 },
+  6: { feedIntake: 30, cumFeed: 136, bodyWt: 170, fcr: 0.80 },
+  7: { feedIntake: 32, cumFeed: 168, bodyWt: 205, fcr: 0.82 },
+  8: { feedIntake: 35, cumFeed: 203, bodyWt: 231, fcr: 0.88 },
+  9: { feedIntake: 39, cumFeed: 242, bodyWt: 265, fcr: 0.91 },
+  10:{ feedIntake: 42, cumFeed: 284, bodyWt: 305, fcr: 0.93 },
+  
+};
