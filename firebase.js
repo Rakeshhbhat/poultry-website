3d773995eb635fb3fcfcebfb0739053e8c2b6a72
athulsnayak <103585243+athulsnayak@users.noreply.github.com>
2026-02-08 14:57:17 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
 export const firebaseApp = initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXXX",
-  appId: "XXXXXXX"
+  apiKey: "AIzaSyDHnDFe-sg7hc4I8jSEHR7wIlHUnLfUA8A",
+  authDomain: "poultry-record.firebaseapp.com",
+  projectId: "poultry-record",
+  storageBucket: "poultry-record.firebasestorage.app",
+  messagingSenderId: "476624930714",
+  appId: "1:476624930714:web:d7847899b8e1f3eb4d5c23"
 });
 
 
