40b0a38eb7959573d4eff57ce4dd66dac36ccb6d
athulsnayak <103585243+athulsnayak@users.noreply.github.com>
2026-02-08 14:56:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+export const firebaseApp = initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXXXXX",
+  appId: "XXXXXXX"
+});
+
 
