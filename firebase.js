f74fa3e043f2165901e202f07c5effd46ba2631d
athulsnayak <103585243+athulsnayak@users.noreply.github.com>
2026-02-08 15:49:19 +0530

    Update storageBucket URL for Firebase configuration

--- firebase.js
+++ firebase.js
@@ -5,7 +5,8 @@ export const firebaseApp = initializeApp({
   apiKey: "AIzaSyDHnDFe-sg7hc4I8jSEHR7wIlHUnLfUA8A",
   authDomain: "poultry-record.firebaseapp.com",
   projectId: "poultry-record",
-  storageBucket: "poultry-record.firebasestorage.app",
+  storageBucket: "poultry-record.appspot.com",
+ //storageBucket: "poultry-record.firebasestorage.app",
   messagingSenderId: "476624930714",
   appId: "1:476624930714:web:d7847899b8e1f3eb4d5c23"
 });
